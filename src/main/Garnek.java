422b536d2021395e6d7ecc237995c3b7588be430
Katarzyna <katarzyna.niziol1993@gmail.com>
2022-04-12 12:41:01 +0200

    Initial commit

---
+++ src/main/Garnek.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Garnek {
+    int srednica;
+    int wysokosc;
+    String kolor;
+
+    public Garnek() {
+    }
+
+    public Garnek(int srednica, int wysokosc, String kolor) {
+        this.srednica = srednica;
+        this.wysokosc = wysokosc;
+        this.kolor = kolor;
+    }
+}
